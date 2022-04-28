a90a2b22c92dd7f15ce6ade19d4578b61596e95c
Xavimat <xaviermatoses@gmail.com>
2022-04-28 15:14:13 +0200

    Nav bar

---
+++ BoostrapYValidacione.js
@@ -0,0 +1,32 @@
+'use strict';
+/**
+ * Exercise Bootstrap and Validation
+ *      for     The Bridge
+ *      by      Shan & Xavi
+ * 2022-04-28
+ */
+
+////////////////////////////////////////////////////////////////////////////////
+// Constants
+const navBtnForm = document.querySelector('#nav-btn-form');
+const navBtnList = document.querySelector('#nav-btn-list');
+
+////////////////////////////////////////////////////////////////////////////////
+// Functions
+
+const showForm = () => {
+    console.log("TODO: Show the form section");
+};
+
+const showList = () => {
+    console.log("TODO: Show the users list section");
+};
+
+////////////////////////////////////////////////////////////////////////////////
+// Listeners
+navBtnForm.addEventListener('click', showForm);
+navBtnList.addEventListener('click', showList);
+
+////////////////////////////////////////////////////////////////////////////////
+// Init
+
