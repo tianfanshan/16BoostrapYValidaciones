3c8181c873a0e27244bc814a517dec347029f2cc
tianfanshan <stf30111994@gmail.com>
2022-04-28 16:03:33 +0200

    guardainformacion

--- BoostrapYValidacione.js
+++ BoostrapYValidacione.js
@@ -13,8 +13,19 @@ const navBtnList = document.querySelector('#nav-btn-list');
 const sectForm = document.querySelector('#form');
 const sectList = document.querySelector('#list');
 
+const nombre = document.getElementById('name')
+const correo = document.getElementById('email')
+const password1 = document.getElementById('password1')
+const password2 = document.getElementById('password2')
+const btn = document.getElementById('btn')
+
+
+
+
+
 ////////////////////////////////////////////////////////////////////////////////
 // Functions
+
 
 const showForm = () => {
     sectForm.classList.remove('d-none');
@@ -26,10 +37,28 @@ const showList = () => {
     sectList.classList.remove('d-none');
 };
 
+const guardarInformacion = () => {
+    
+    let database = JSON.parse(localStorage.getItem('myForm')) || [];
+
+    let information ={
+        nombre:nombre.value,
+        correo:correo.value,
+        password:password1.value
+    }
+    
+    database.push(information)
+
+    localStorage.setItem('myForm', JSON.stringify(database))
+}
+
+
+
 ////////////////////////////////////////////////////////////////////////////////
 // Listeners
 navBtnForm.addEventListener('click', showForm);
 navBtnList.addEventListener('click', showList);
+btn.addEventListener('click', guardarInformacion);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Init
